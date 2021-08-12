4f85b0072a7961ad9997516e7949fa546b063740
FeHeap <82155803+FeHeap@users.noreply.github.com>
2021-08-13 04:55:28 +0800

    callback

--- app_core.py
+++ app_core.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 
 app = Flask(__name__)
 
+line_bot_api = LineBotApi('U2a11299ffe4f96bec2317e639f5d7bfa')
+handler = WebhookHandler('c6f785a785124a0c818fc3aaf203fb82')
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
 
 
 if __name__ == '__main__':
