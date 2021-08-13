13a8474f6c4843b421e970f834df5bfd493b6bbb
FeHeap <82155803+FeHeap@users.noreply.github.com>
2021-08-14 02:52:01 +0800

    camp_form

---
+++ app/forms_camp.py
@@ -0,0 +1,27 @@
+# -*- coding = utf-8 -*-
+# @Time: 2021/8/14 上午 02:29
+# @Software: PyCharm
+
+date_to_link = {
+    '8/14':'https://forms.gle/tJ8i4ZdCvg9k9aNfA',
+    '8/16':'https://forms.gle/RPuBV7VG2FNXmWPz6',
+    '8/17': 'https://forms.gle/uzyR7bZwgwdptiHQ7',
+    '8/18': 'https://forms.gle/oHSyLkisZdigEJWu8',
+    '8/19': 'https://forms.gle/GsJHUhpLbdGjGLXJ8',
+    '8/20': 'https://forms.gle/DbkxwRrxP11wqdkK7',
+    '8/23': 'https://forms.gle/Dr7rLpcJVKm7RJXC9',
+    '8/24': 'https://forms.gle/3Bpdawk6mHgiM9x67',
+    '8/25': 'https://forms.gle/YigdRX1o4BpZzK136',
+    '8/26': 'https://forms.gle/G9yE3UCbuNAJsHkd8',
+    '8/27': 'https://forms.gle/oHKGGpU1MRYSi3L59',
+    '8/30': 'https://forms.gle/TRHYoVjCnXWVLjHR8',
+    '8/31': 'https://forms.gle/UftkNMRNEy2HWzAs8',
+    '9/1': 'https://forms.gle/GB1VBa7zJCXpytne8',
+    '9/2': 'https://forms.gle/6ezFaJ8uQGPooJL26',
+    '9/3': 'https://forms.gle/8bv6SYWtmtPbHrWa6',
+    '9/6': 'https://forms.gle/HbMjzkJpFjGbodYz5',
+    '9/7': 'https://forms.gle/oBxWWZcnBp727XmeA',
+    '9/8': 'https://forms.gle/DJmCK32aLDwiFX94A',
+    '9/9': 'https://forms.gle/fhVu9cPATtfoEhYq9',
+    '9/10': 'https://forms.gle/Ye7Ccjkt7qY7DYLp7',
+}
--- app/models_for_line.py
+++ app/models_for_line.py
@@ -5,11 +5,18 @@
 from app import line_bot_api, handler
 
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import forms_camp
 
 @handler.add(MessageEvent, message=TextMessage)
 def Reply(event):
-    print("user_id",event.source.user_id)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+    print("user_id:",event.source.user_id)
+    if event.source.user_id == 'U2a11299ffe4f96bec2317e639f5d7bfa':
+        command = str.strip(event.message.text)
+        if command[0:2] == '表單':
+            command = str.strip(command[2:])
+            if command in forms_camp.date_to_link:
+                message_to_client = '今日的簽到表單\n' + forms_camp.date_to_link[command]
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=message_to_client)
+                )
